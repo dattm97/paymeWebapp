00b1fb97184285973f9f0562ee8aeb04b39390f5
dattm <dattm@edoctor.vn>
2022-08-21 17:47:38 +0700

    added: add thêm file account constant

--- src/action/openWallet.js
+++ src/action/openWallet.js
@@ -1,14 +1,14 @@
 import {WALLET_ACTIONS} from '../constant/walletAction.constant';
 import {encrypt} from '../helper';
 
-export const createOpenWalletURL = async ({domain, config}) => {
+export const createOpenWalletURL = ({domain, config}) => {
   const configs = {
     ...config,
     actions: {
       type: WALLET_ACTIONS.OPEN_WALLET,
     },
   };
-  const encrypted = await encrypt(configs);
+  const encrypted = encrypt(configs);
 
   return 'getDataWithAction/' + encodeURIComponent(encrypted);
 };
--- src/services/StaticModule/useStaticServer.js
+++ src/services/StaticModule/useStaticServer.js
@@ -11,13 +11,13 @@ const useStaticServer = ({moduleName, suffix}) => {
       const startServer = async () => {
         const path = RNFS.DocumentDirectoryPath + `/${moduleName}/`;
         console.log('DEBUG_USE_STATIC_SERVER: ', path);
-        const _server = new StaticServer(3000, path, {
+        const _server = new StaticServer(0, path, {
           localOnly: true,
           keepAlive: true,
         });
         _server.start().then(_url => {
           if (suffix) {
-            setUrl(`${_url}`);
+            setUrl(`${_url}/${suffix}`);
           } else {
             setUrl(`${_url}`);
           }
